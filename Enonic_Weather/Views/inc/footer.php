edb96d16bdd9f659d43c09bae83acd78bdcb6cf9
Ivo <tfury@hotmail.com>
2018-10-21 15:55:12 +0200

    Ferdig

--- Enonic_Weather/Views/inc/footer.php
+++ Enonic_Weather/Views/inc/footer.php
@@ -1,1 +1,9 @@
-<div class="footer"></div>
+<div class="footer">
+    <div class="container">
+        <h4>Info:</h4>
+        <ul>
+            <li>Opprettet 21.10.2018</li>
+            <li>Author Ivo K &#169;</li>
+        </ul>
+    </div>
+</div>
--- Enonic_Weather/Views/pages/index.php
+++ Enonic_Weather/Views/pages/index.php
@@ -41,7 +41,7 @@
         </div>
     </div>
 
-    <footer> <?php include_once "../inc/footer.php" ?> </footer>
+    <footer><?php include_once "../inc/footer.php" ?></footer>
     <!-- scripts -->
     <script src="../../bower_components/moment/moment.js"></script>
     <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
