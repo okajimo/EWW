37c3c9ecb03a5e94ae004ec980a25bb63199ac7f
Ivo <tfury@hotmail.com>
2018-10-21 22:00:45 +0200

    :)

--- Enonic_Weather/Views/pages/index.php
+++ Enonic_Weather/Views/pages/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Eonic weather</title>
+    <title>Enonic weather</title>
     <link rel="icon" href="../../img/logo.png" type="image/gif" sizes="16x16">
     
     <!-- css -->
