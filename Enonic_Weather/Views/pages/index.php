e32ffda4f8f8735791330d8acec57cf8fabbfed6
Ivo <tfury@hotmail.com>
2018-10-21 09:09:39 +0200

    start

---
+++ Enonic_Weather/Views/pages/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nb">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eonic weather</title>
+    <link rel="icon" href="../../img/logo.png" type="image/gif" sizes="16x16">
+    
+    <!-- css -->
+    <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../Styles/custom.css">
+</head>
+<body>
+    <nav><?php include_once "../inc/navbar.php" ?></nav>
+
+    <div id="main" class="container">
+        
+    </div>
+
+    <footer> <?php include_once "../inc/footer.php" ?> </footer>
+    <!-- scripts -->
+    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../../js/custom.js"></script>
+</body>
+</html>
